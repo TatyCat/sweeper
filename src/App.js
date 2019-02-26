62c2537384178bda59e1e5870d5caecff32b2c47
TatyCat <williams.tatyanna@gmail.com>
2019-02-26 18:26:40 -0500

    removed commented out code in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 import GameBoard from './components/GameBoard'
-// import axios from 'axios'
 
 
 class App extends Component {
-  // componentDidMount() {
-  //   axios
-  //     .post('https://minesweeper-api.herokuapp.com/games', {
-  //       difficulty: 0
-  //     })
-  //     .then(resp => {
-  //       console.log({ resp })
-  //     })
-  // }
+
 
   render() {
     return (
